855226bd5e49dadc2f53d66ee05c7bbe0a0591c2
scada-josh <scada.josh@gmail.com>
2019-02-25 03:20:48 +0700

    PostgreSQL - select command - debug echo test

--- postgresql-exam/select-cmd.php
+++ postgresql-exam/select-cmd.php
@@ -1,14 +1,16 @@
 <?php
 
-$host = 'ec2-54-227-246-152.compute-1.amazonaws.com';
-$database = 'd2kem2r4vb3nre';
-$username = 'pwyrlnoahiovqp';
-$password = 'fcc2d652fec73b737a042e8099ed83970b641742c2c8db2200b41268f5e2332b';
+  echo "Select CMD";
 
-$connection = new PDO(sprintf('pgsql:host=%s;dbname=%s', $host, $database), $username, $password);
+// $host = 'ec2-54-227-246-152.compute-1.amazonaws.com';
+// $database = 'd2kem2r4vb3nre';
+// $username = 'pwyrlnoahiovqp';
+// $password = 'fcc2d652fec73b737a042e8099ed83970b641742c2c8db2200b41268f5e2332b';
 
-echo($connection);
-echo("Helloworld");
+// $connection = new PDO(sprintf('pgsql:host=%s;dbname=%s', $host, $database), $username, $password);
 
-$query = "SELECT * FROM appointments";
-$connection->execute($query);
+// echo($connection);
+// echo("Helloworld");
+
+// $query = "SELECT * FROM appointments";
+// $connection->execute($query);
